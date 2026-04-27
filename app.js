a9800e75080cbc516cfd0873901ce8f6b571bb89
alestainer <16204439+Alestainer@users.noreply.github.com>
2026-04-27 12:19:59 -1000

    Dark map tiles, full-opacity text, pill-shaped buttons

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ const state = {
 };
 
 const map = L.map("map").setView([20, 0], 2);
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
+  subdomains: "abcd",
   maxZoom: 19,
 }).addTo(map);
 
